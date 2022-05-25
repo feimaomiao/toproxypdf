fce434ebbe76e33b34bf426c9c706346e81d13fe
Matthew Lam <lcpmatthew@gmail.com>
2022-05-24 19:17:46 -0700

    testing

--- __main__.py
+++ __main__.py
@@ -111,8 +111,8 @@ def list_files(arguments: dict) -> list:
             img.verify()
             img = Image.open(fn)
             # resizing images into 1000dpi 
-            img = img.resize((2500,3500))
-            img = add_corners(img, 120)
+            img = img.resize((635,889))
+            img = add_corners(img, 30)
             allfiles.append(img)
         except Exception as e:
             logging.info(f"{fn} is not a valid image, will be skipped ({type(e)})")
@@ -131,7 +131,7 @@ def generate_images(fileslist: list,arguments: dict) -> list:
         list: list of white background images
     """
     # round up amount of pages needed, create however many white backgrounds
-    backgrounds = [Image.new("RGBA", (8270,11690), color="white") for i in range(-(-len(fileslist) // 9 ))]
+    backgrounds = [Image.new("RGBA", (1660,2340), color="white") for i in range(-(-len(fileslist) // 9 ))]
     logging.debug("Created background images")
     # balanced x/y coordinates
     xcords = [385, 2885, 5385]
@@ -147,7 +147,7 @@ def generate_images(fileslist: list,arguments: dict) -> list:
         y = ycords[(i % 9) // 3]
         backgrounds[i//9].paste(fileslist[i], (x, y), fileslist[i])
         logging.debug(f"Pasted {fileslist[i]} onto background {i // 9}")
-    logging.info(f"Finished pasting images, converting {len()}to RGB from RGBA")
+    logging.info(f"Finished pasting images, converting {len(fileslist)}to RGB from RGBA")
     return [i.convert("RGB") for i in backgrounds]
 
 
